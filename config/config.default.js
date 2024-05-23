c5fcccf4ae78444bb1ee194c685688c95d3cb11e
fannong <13802882756@139.com>
2024-05-23 16:25:05 +0800

    test jenkins

--- config/config.default.js
+++ config/config.default.js
@@ -27,33 +27,33 @@ module.exports = (appInfo) => {
       // convert: false,
       // validateRoot: false,
     },
-    redis: {
-      client: {
-        port: 6379,
-        host: "127.0.0.1",
-        password: "",
-        db: 0,
-      },
-    },
-    mysql: {
-      // 单数据库信息配置
-      client: {
-        // host
-        host: "127.0.0.1",
-        // 端口号
-        port: "3306",
-        // 用户名
-        user: "root",
-        // 密码
-        password: "root",
-        // 数据库名
-        database: "fannong",
-      },
-      // 是否加载到 app 上，默认开启
-      app: true,
-      // 是否加载到 agent 上，默认关闭
-      agent: false,
-    },
+    // redis: {
+    //   client: {
+    //     port: 6379,
+    //     host: "127.0.0.1",
+    //     password: "",
+    //     db: 0,
+    //   },
+    // },
+    // mysql: {
+    //   // 单数据库信息配置
+    //   client: {
+    //     // host
+    //     host: "127.0.0.1",
+    //     // 端口号
+    //     port: "3306",
+    //     // 用户名
+    //     user: "root",
+    //     // 密码
+    //     password: "root",
+    //     // 数据库名
+    //     database: "fannong",
+    //   },
+    //   // 是否加载到 app 上，默认开启
+    //   app: true,
+    //   // 是否加载到 agent 上，默认关闭
+    //   agent: false,
+    // },
     development: {
       watchDirs: ["app", "lib", "config", "app.js", "agent.js", "config.default.js"],
       overrideDefault: true,
