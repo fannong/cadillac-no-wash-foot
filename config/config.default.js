09ca89b0fdfc000f20175332f8bccc0da5c055a7
fannong <13802882756@139.com>
2024-05-23 17:10:45 +0800

    test mysql

--- config/config.default.js
+++ config/config.default.js
@@ -35,25 +35,25 @@ module.exports = (appInfo) => {
         db: 0,
       },
     },
-    mysql: {
-      // 单数据库信息配置
-      client: {
-        // host
-        host: "127.0.0.1",
-        // 端口号
-        port: "3306",
-        // 用户名
-        user: "root",
-        // 密码
-        password: "root",
-        // 数据库名
-        database: "fannong",
-      },
-      // 是否加载到 app 上，默认开启
-      app: true,
-      // 是否加载到 agent 上，默认关闭
-      agent: false,
-    },
+    // mysql: {
+    //   // 单数据库信息配置
+    //   client: {
+    //     // host
+    //     host: "127.0.0.1",
+    //     // 端口号
+    //     port: "3306",
+    //     // 用户名
+    //     user: "root",
+    //     // 密码
+    //     password: "root",
+    //     // 数据库名
+    //     database: "fannong",
+    //   },
+    //   // 是否加载到 app 上，默认开启
+    //   app: true,
+    //   // 是否加载到 agent 上，默认关闭
+    //   agent: false,
+    // },
     development: {
       watchDirs: ["app", "lib", "config", "app.js", "agent.js", "config.default.js"],
       overrideDefault: true,
