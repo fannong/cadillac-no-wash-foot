66d5e20a08ecf71510f39ef949edd7f3501cd5c1
fannong <13802882756@139.com>
2024-06-07 16:05:59 +0800

    abandom schedule

--- app/schedule/watchFile.js
+++ app/schedule/watchFile.js
@@ -1,17 +1,17 @@
-const Subscription = require("egg").Subscription;
+// const Subscription = require("egg").Subscription;
 
-let i = 0;
+// let i = 0;
 
-class WatchFile extends Subscription {
-  static get schedule() {
-    return {
-      interval: "1s",
-      type: "all",
-    };
-  }
-  async subscribe() {
-    // console.log("watch file", i++);
-  }
-}
+// class WatchFile extends Subscription {
+//   static get schedule() {
+//     return {
+//       interval: "1s",
+//       type: "all",
+//     };
+//   }
+//   async subscribe() {
+//     // console.log("watch file", i++);
+//   }
+// }
 
-module.exports = WatchFile;
+// module.exports = WatchFile;
