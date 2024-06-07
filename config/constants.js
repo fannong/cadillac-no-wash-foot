9cbf084a19403ce235efff12037744a8ad3bc438
fannong <13802882756@139.com>
2024-06-07 18:30:01 +0800

    change  uesrtable name

--- config/constants.js
+++ config/constants.js
@@ -4,7 +4,7 @@
 // 3. 为了方便修改这些常量，只需要修改一个文件即可
 
 module.exports = {
-  theUserTable: "my_new_table",
+  theUserTable: "user_table",
   theCrudTable: "crud_table",
   emailAccount: "miaowu11@163.com", // 用来发邮件的邮件账户。一般是开发者自己的邮箱或者企业的邮箱
   // emailAuthorizationCode: "HBPQPAPMIBYUHLIR", // 14邮箱  11邮箱：HMDJXCHHYITNRQDS
